2dc7387cd5f201140adfbea7e205c7ad99084e18
4hg <33138892+4hg@users.noreply.github.com>
2022-09-25 22:01:47 -0500

    add finished types and functions

--- knight.rb
+++ knight.rb
@@ -1,11 +1,13 @@
 require_relative 'types.rb'
-require_relative 'functions.rb'
 
-case $*.first
-when '-e'
-  $*.last
-when '-f'
-  File.read($*.last)
-else
-  abort 'usage: knight.rb (-e 'source code' | -f filename)'
-end if $*.size == 2
+# only -e sort of works
+if __FILE__ == $0
+  case $*.first
+  when '-e'
+    $*.last.dup.parse!.call
+  when '-f'
+    File.read($*.last)
+  else
+    abort "usage: knight.rb (-e 'source code' | -f filename)"
+  end if $*.size == 2
+end
--- types.rb
+++ types.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
+require_relative 'functions.rb'
+
+class Object
+  alias call itself
+  def to_i        = call.to_i
+  def to_s        = call.to_s
+  def to_a        = call.to_a
+  def coerce(rhs) = [rhs, to_i]
+end
+
+class NilClass
+  def truthy? = false
+  def inspect = 'null'
+  freeze
+end
+
 class TrueClass
   alias truthy? itself
   def to_i    = 1
@@ -18,36 +34,50 @@ class FalseClass
   freeze
 end
 
-class NilClass
-  def truthy? = false
-  def inspect = 'null'
+class Integer
+  def to_a    = positive? ? digits.reverse : abs.digits.reverse.map(&:-@)
+  def truthy? = self != 0
   freeze
 end
 
 class String
   alias to_a chars
   alias old_to_i to_i
-  def to_i    = self.sub(/d/i, '@').old_to_i
+  def to_i    = sub(/d/i, '@').old_to_i
   def truthy? = !empty?
   prepend Module.new {
     def +(rhs)  = super(rhs.to_s)
     def *(rhs)  = super(rhs.to_i)
-    def <(rhs)  = super(rhs.to_i)
-    def >(rhs)  = super(rhs.to_i)
-    def ==(rhs) = rhs.is_a?(String) && super(rhs.to_i)
+    def <(rhs)  = super(rhs.to_s)
+    def >(rhs)  = super(rhs.to_s)
+    def ==(rhs) = rhs.is_a?(String) && super(rhs)
   }
   def set(b, l, s)
     ns = dup
     ns[b, l] = s.to_s
     ns
   end
+
+  def parse!
+    case
+    when slice!(/\A([():\s]+|#.*\n)/) then return replace($').parse!
+    when slice!(/\A\d+/) then $&.to_i
+    when slice!(/\A[a-z_][a-z_\d]*/) then Identifier.new($&)
+    when slice!(/\A(?:'([^']*?)'|"([^"]*?)")/) then $+
+    when slice!(/\A([TF])[A-Z_]*/) then $1 == 'T'
+    when slice!(/\AN[A-Z_]*/) then nil
+    when slice!(/\A@/) then []
+    when slice!(/\A([A-Z][A-Z_]*|.)/) then return FNS.fetch($&[0]).create{ parse! }
+    end.tap{ replace $' }
+  end
   freeze
 end
 
 class Array
+  include Comparable
   alias to_i size
   def truthy? = !empty?
-  def to_s    = self.join $/
+  def to_s    = join $/
   def *(rhs)
     r = []
     rhs.to_i.times{ r += self }
@@ -56,9 +86,9 @@ class Array
   def **(rhs) = join rhs.to_s
   prepend Module.new {
     def +(rhs)  = super(rhs.to_a)
-    def <(rhs)  = super(rhs.to_i)
-    def >(rhs)  = super(rhs.to_i)
-    def ==(rhs) = rhs.is_a?(Array) && super(rhs.to_i)
+    def <(rhs)  = super(rhs.to_a)
+    def >(rhs)  = super(rhs.to_a)
+    def ==(rhs) = rhs.is_a?(Array) && super(rhs)
   }
   def set(b, l, a)
     na = dup
@@ -68,23 +98,14 @@ class Array
   freeze
 end
 
-class Integer
-  def to_a    = positive? ? digits.reverse : abs.digits.reverse.map(&:-@)
-  def truthy? = self != 0
-  prepend Module.new{
-    def +(rhs)  = super(rhs.to_i)
-    def -(rhs)  = super(rhs.to_i)
-    def *(rhs)  = super(rhs.to_i)
-    def /(rhs)  = super(rhs.to_i)
-    def %(rhs)  = super(rhs.to_i)
-    def **(rhs) = super(rhs.to_i).to_i
-    def <(rhs)  = super(rhs.to_i)
-    def >(rhs)  = super(rhs.to_i)
-    def ==(rhs) = rhs.is_a?(Integer) && super(rhs.to_i)
-  }
-  freeze
-end
-
 class Identifier
-
+  @vs = {}
+  def self.new(name) = @vs[name] ||= super()
+  attr_accessor :call
+  def truthy? = call.truthy?
 end
+
+class Proc
+  def create(&b) = arity.times.map(&b).then { |x| proc { call(x) } }
+  def truthy? = !call
+end 
